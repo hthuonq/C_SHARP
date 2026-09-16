3c7002bc0429f627c6f6dc40a8d74fc1e272dfaf
hthuonq <dole120406@gmail.com>
2026-09-16 11:46:14 +0700

    TUAN 01/ LAB 01/ BUỔI 2, 7, 9 / hoàn thành bài tập nhóm

---
+++ TUAN01/BUOI2/bai1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BUOI2
+{
+    class bai1
+    {
+        public static void chay()
+        {
+            int a, b;
+            int tong;
+
+            Console.Write("Nhap a, b: ");
+            string[] num  = Console.ReadLine().Split(); // nhập dữ liệu rồi tách chuỗi
+
+            // chuyển phần tử thành số nguyên
+            a = int.Parse(num[0]); // num[0] : vị trí đầu tiên của mảng
+            b = int.Parse(num[1]);
+
+            tong = b * (b + 1) / 2 - (a - 1) * a / 2;
+            Console.WriteLine("tong cua cac so trong doan [{0}, {1}] là {2} ", a, b, tong); // {0},{1},{2} là vị trí để đưa giá trị biến vào chuỗi.
+        }
+    }
+}
---
+++ TUAN01/BUOI9/main.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BUOI9
+{
+    class main
+    {
+        public static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("BUOI 9");
+                Console.WriteLine("1. liet ke so chan");
+                Console.WriteLine("2. tim gia tri chan dau tien, cuoi cung");
+                Console.WriteLine("3. dem dau");
+                Console.WriteLine("4. tim kiem");
+                Console.WriteLine("0. thoat");
+
+                Console.WriteLine("chon bai: ");
+                choice = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                switch (choice)
+                {
+                    case 1:
+                        bai1.chay();
+                        break;
+                    case 2:
+                        bai2.chay();
+                        break;
+                    case 3:
+                        bai3.chay();
+                        break;
+                    case 4:
+                        bai4.chay();
+                        break;
+                    case 0:
+                        Console.WriteLine("ket thuc");
+                        break;
+                    default:
+                        Console.WriteLine("lua chon khong hop le");
+                        break;
+                }
+                if (choice != 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("nhan Enter de quay lai menu");
+                    Console.ReadLine();
+
+                }
+
+            } while (choice != 0);
+        }
+    }
+}
